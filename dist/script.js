f8f7c38092bf196c4e0e4e85f28ecaa492e028ea
jeffreyshen19 <jshen@andover.edu>
2018-04-15 13:57:47 -0400

    update map

--- dist/script.js
+++ dist/script.js
@@ -5,6 +5,62 @@ function generateTooltip(json){
 
 
 /*
+  US Map
+*/
+
+/*d3.selectAll(".map").each(function(){
+  var url = this.dataset.url;
+  var currentElement = this;
+
+  d3.xml(url, "image/svg+xml", function(xml) {
+    currentElement.appendChild(xml.documentElement);
+    //styleImportedSVG();
+  });
+});*/
+
+d3.svg("/assets/graphics/map.svg").then(function(res){
+  var svg = res.documentElement;
+  d3.select("#us_map").node().appendChild(svg);
+});
+
+/*.get(function(error, xml) {
+  //if (error) throw error;
+
+});
+
+
+/*
+
+  var htmlYes = generateTooltip({title: "Yes", responses: yesResponses, percentage: yesResponses / (yesResponses + noResponses)}),
+      htmlNo = generateTooltip({title: "No", responses: noResponses, percentage: noResponses / (yesResponses + noResponses)});
+
+  var tooltip = d3.select(this.firstChild);
+
+  var yesX = parseInt(d3.select(this.children[1]).node().style.width.replace("%", ""));
+  var noX = parseInt(d3.select(this.children[2]).node().style.width.replace("%", ""));
+
+  d3.select(this.children[1]) //If hovering over yes
+    .on("mouseover", function(d){
+      tooltip.classed("hidden", false).html(htmlYes)
+        .style("left", "calc(" + Math.round(yesX / 2) + "% - " + Math.round(tooltip.node().offsetWidth / 2) + "px)")
+        .style("top", "-" + (Math.round(tooltip.node().offsetHeight) + 10) + "px");
+    })
+    .on("mouseout", function(d){
+      tooltip.classed("hidden", true);
+    });
+
+  d3.select(this.children[2]) //If hovering over no
+    .on("mouseover", function(d){
+      tooltip.classed("hidden", false).html(htmlNo)
+        .style("left", "calc(" + Math.round(yesX + noX / 2) + "% - " + Math.round(tooltip.node().offsetWidth / 2) + "px)")
+        .style("top", "-" + (Math.round(tooltip.node().offsetHeight) + 10) + "px");
+    })
+    .on("mouseout", function(d){
+      tooltip.classed("hidden", true);
+    });
+});*/
+
+/*
   Percentage Slider
 */
 
