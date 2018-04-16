cd082824242c91070c904b55a71f646bc3ca8140
jeffreyshen19 <jshen@andover.edu>
2018-04-15 22:56:53 -0400

    refactoring map.js

--- dist/script.js
+++ dist/script.js
@@ -9,10 +9,9 @@ function generateTooltip(json){
 */
 
 d3.selectAll(".map").each(function(){
-  //TODO: reorganize these variables
-  var url = this.dataset.url;
-  var total = 0;
-  var regionNames = ["Discontinuous", "Northeast", "Southeast", "Southwest", "West", "Midwest", "International"];
+  var total = 0,
+      regionNames = ["Discontinuous", "Northeast", "Southeast", "Southwest", "West", "Midwest", "International"];
+
   var responses = this.dataset.responses.split(",").map(function(element, i){
     total += parseInt(element);
     return {
@@ -21,11 +20,11 @@ d3.selectAll(".map").each(function(){
     };
   });
 
-  var currentElement = this;
-  var tooltipText;
-  var tooltip = d3.select(this.firstChild);
+  var currentElement = this,
+      tooltipText,
+      tooltip = d3.select(this.firstChild);
 
-  d3.svg(url).then(function(res){
+  d3.svg(this.dataset.url).then(function(res){
     var svg = res.documentElement;
     d3.select(currentElement).node().appendChild(svg);
     d3.select(svg).selectAll("*")
@@ -33,17 +32,8 @@ d3.selectAll(".map").each(function(){
       .on("mouseover", function(d, i){
         tooltipText = generateTooltip({title: d.name, responses: d.responses, percentage: d.responses / total});
         tooltip.classed("hidden", false).html(tooltipText);
-
-        console.log(d3.select(currentElement).node().getBoundingClientRect());
-
-        var bbox = d3.select(currentElement).node().getBoundingClientRect();
-        var mouse = d3.mouse(this);
-
-        ///tooltip.style("left", mouse[0] - bbox.x + "px")
-          //.style("top", mouse[1] - bbox.y + "px");
       })
       .on("mousemove", function(d){
-        var bbox = d3.select(currentElement).node().getBoundingClientRect();
         var mouse = d3.mouse(currentElement);
 
         tooltip.style("left", mouse[0] - Math.round(tooltip.node().offsetWidth / 2) + "px")
