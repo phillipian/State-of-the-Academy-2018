aacc1c242dc763520cbbf9fc1ecb88a1a94da027
jeffreyshen19 <jshen@andover.edu>
2018-04-16 15:59:18 -0400

    add pie chart

--- dist/script.js
+++ dist/script.js
@@ -99,3 +99,78 @@ var percentageSliders = d3.selectAll(".percentage-slider").each(function(){
 /*
   Pie Chart
 */
+
+var pieCharts = d3.selectAll(".pie").each(function(){
+  var responses = this.dataset.responses.split(","),
+      labels = this.dataset.labels.split(","),
+      accent = d3.color(this.dataset.accent);
+
+  var width = 300,
+      height = 300,
+      radius = 150;
+
+  var total = 0;
+  var piedata = responses.map(function(d, i){
+    total += parseInt(d);
+    if(i != 0) accent = d3.color(accent.darker());
+    return {
+      label: labels[i],
+      value: parseInt(d),
+      color: d3.rgb(accent)
+    };
+  });
+
+  var colors = d3.scaleOrdinal(d3.schemeCategory20c);
+
+  var pie = d3.pie()
+    .value(function(d){
+      return d.value;
+    });
+
+  var arc = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(radius / 2);
+
+  var tooltip = d3.select(this.firstChild),
+      tooltipText,
+      mouse;
+
+  var currentElement = this;
+
+  //Add pieChart
+  var myChart = d3.select(this).append("svg")
+    .attr("width", "100%")
+    .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + (width - radius) + "," + (height - radius) + ")")
+      .selectAll("path").data(pie(piedata))
+      .enter().append("path")
+        .attr("fill", function(d, i){
+          return d.data.color;
+        })
+        .attr("stroke", "white")
+        .attr("d", arc)
+        .on("mouseover", function(d, i){
+          tooltipText = generateTooltip({title: d.data.label, responses: d.value, percentage: d.value / total});
+          tooltip.classed("hidden", false).html(tooltipText);
+        })
+        .on("mousemove", function(d){
+          mouse = d3.mouse(currentElement);
+
+          tooltip.style("left", mouse[0] - Math.round(tooltip.node().offsetWidth / 2) + "px")
+            .style("top", mouse[1] - Math.round(tooltip.node().offsetHeight) - 12 + "px");
+        })
+        .on("mouseout", function(d){
+          tooltip.classed("hidden", true);
+        });
+
+  //Add labels underneath pie chart
+  d3.select(this).append("div")
+    .attr("class", "pie-label")
+    .attr("width", "100%")
+    .selectAll("p").data(piedata)
+    .enter().append("p")
+      .html(function(d, i){
+        return "<div class = 'bubble' style = 'background:" + d.color + "'></div>" + d.label;
+      });
+});
